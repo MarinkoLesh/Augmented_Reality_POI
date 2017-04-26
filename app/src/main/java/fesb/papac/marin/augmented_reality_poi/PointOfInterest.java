50e18d9d1cd95bf07ec341ac3d8d8a22a088df98
MarinkoLesh <marin.papac.dbk@gmail.com>
2017-04-26 15:58:38 +0200

    JSON added to assets

---
+++ app/src/main/java/fesb/papac/marin/augmented_reality_poi/PointOfInterest.java
@@ -0,0 +1,29 @@
+package fesb.papac.marin.augmented_reality_poi;
+
+/**
+ * Created by Marin on 26.4.2017..
+ */
+
+public class PointOfInterest {
+
+    public double latitude;
+    public double longitude;
+    public double altitude;
+    public String place;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public String getPlaces() {
+        return place;
+    }
+}
